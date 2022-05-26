3127392b89c0045277133ab7d3ca0919d13ef312
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2022-05-26 23:19:33 +0700

    create category section

---
+++ script/script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiperHeader", {
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".bx-chevron-right-circle",
+      prevEl: ".bx-chevron-left-circle",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+     },
+
+     autoplay: {
+        delay: 1200
+    }
+    
+  });
+
+$(document).ready(function(){
+    $('.btn-slide1').click(function(){
+        $('.btn-slide1').addClass('bxs-chevron-left-circle')
+        $('.btn-slide2').removeClass('bxs-chevron-right-circle')
+
+    })
+
+    $('.btn-slide2').click(function(){
+        $('.btn-slide2').addClass('bxs-chevron-right-circle')
+        $('.btn-slide1').removeClass('bxs-chevron-left-circle')
+    })
+})
